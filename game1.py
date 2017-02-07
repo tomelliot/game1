7b78bfac8ce7931c9cef630085abdfc24bdc204a
Tom Elliot <elliot.tom@gmail.com>
2017-02-07 15:32:48 +1100

    added functionality for selecting a point

--- game1.py
+++ game1.py
@@ -10,6 +10,7 @@ socketio = SocketIO(app)
 new_game_state = {"current_turn": "A",
                                 "player": "A",
                                 "new_game": False,
+                                "selected": [],
                                 "c1": "A", "d1": "B", "e1": "blank", "f1": "blank", "g1": "blank", "h1": "blank", "i1": "blank", "j1": "blank", "k1": "blank",
                                 "b2": "blank", "c2": "blank", "d2": "blank", "e2": "blank", "f2": "blank", "g2": "blank", "h2": "blank", "i2": "blank", "j2": "blank", "k2": "blank", 
                                 "a3": "blank", "b3": "blank", "c3": "blank", "d3": "blank", "e3": "blank", "f3": "blank", "g3": "blank", "h3": "blank", "i3": "blank", "j3": "blank", "k3": "blank", 
@@ -30,6 +31,16 @@ def next_player(player):
         return "A"
     return "uhoh"
 
+@app.route("/new_click/<player>/<point>/", methods=["POST"])
+def new_click(player, point):
+    print request.get_json()
+    if (game_state["selected"] == point):
+        game_state["selected"] = ""
+    else:
+        game_state["selected"] = point
+    socketio.emit('update_board', game_state)
+    return "OK"
+
 @app.route("/new_move/<player>/<point>/", methods=["POST"])
 def new_move(player, point):
     print request.get_json()
@@ -37,8 +48,6 @@ def new_move(player, point):
     # if player != game_state["current_turn"]:
     #     valid_move = False
     if valid_move:
-        # print player
-        # print point
         game_state[point] = player
         game_state["current_turn"] = next_player(player)
     socketio.emit('update_board', game_state)
@@ -47,3 +56,4 @@ def new_move(player, point):
 if __name__ == "__main__":
     # app.run(debug=True)
     socketio.run(app, debug=True)
+    # socketio.run(app, host="0.0.0.0", debug=True)
