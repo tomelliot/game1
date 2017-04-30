9847eb5865e551904adee5088bde615d84c8a48b
Tom Elliot <elliot.tom@gmail.com>
2017-04-30 20:16:39 +1000

    Changed get_user to return first result

--- game1/db_api.py
+++ game1/db_api.py
@@ -12,6 +12,7 @@ default_new_game_state = {"current_turn": "A",
                                 "setup": True,
                                 "magics_to_place":3,
                                 "selected": "",
+                                "result": "",
                                 "c1": empty_point, "d1": empty_point, "e1": empty_point, "f1": empty_point, "g1": empty_point, "h1": empty_point, "i1": empty_point, "j1": empty_point, "k1": empty_point,
                                 "b2": empty_point, "c2": empty_point, "d2": empty_point, "e2": empty_point, "f2": empty_point, "g2": empty_point, "h2": empty_point, "i2": empty_point, "j2": empty_point, "k2": empty_point, 
                                 "a3": empty_point, "b3": empty_point, "c3": empty_point, "d3": empty_point, "e3": empty_point, "f3": empty_point, "g3": empty_point, "h3": empty_point, "i3": empty_point, "j3": empty_point, "k3": empty_point, 
@@ -46,8 +47,8 @@ def get_game_state(game_id):
     game = models.Game.query.filter_by(id=game_id).first()
     return json.loads(game.game_state)
 
-def get_user_list():
-    return users.keys()
+# def get_user_list():
+#     return users.keys()
 
 def get_users():
     return models.User.query.all()
@@ -59,7 +60,7 @@ def get_user(id=None, username=None):
     if id:
         return models.User.query.filter_by(id=id).first()
     if username:
-        return models.User.query.filter_by(username=username)
+        return models.User.query.filter_by(username=username).first()
     return None
 
 def get_users_game_list(user):
